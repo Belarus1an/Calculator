252de11b0e7801b0fa10c6eb027f302807c8f0d2
Belarus1an <42100450+Belarus1an@users.noreply.github.com>
2018-10-21 19:41:18 +0200

    commt2

--- src/CalcMethotd.java
+++ src/CalcMethotd.java
@@ -1,4 +1,63 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class CalcMethotd {
+
+    private static Scanner scan = new Scanner(System.in);
+
+    public static double getNum(){
+
+        if (!scan.hasNextDouble()) {
+            System.out.println("Błąd! Spróbuj ponownie");
+            scan.next();
+            return getNum();
+        } else {
+            return scan.nextDouble();
+        }
+    }
+
+    public static char getOperation (){
+        System.out.println("Wprowadź operację( + , - , * , / )");
+        char operation;
+        operation = scan.next().charAt(0);
+        switch (operation){
+            case '+': return operation;
+
+            case '-': return operation;
+
+            case '*': return operation;
+
+            case '/': return operation;
+
+            default:
+                System.out.println("Błąd! Spróbuj ponownie");
+                return getOperation();
+        }
+    }
+
+    public static double calc(double num1, double num2, char operation){
+
+        double result;
+
+        switch (operation) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                if (num2 != 0) {
+                    result = num1 / num2;
+                    break;
+                } else
+                    System.out.println("Dzielenie na 0!");
+                    System.exit(0);
+            default:
+                result = calc(getNum(), getNum(), getOperation());
+        }
+        return result;
+    }
 }
--- src/Calculator.java
+++ src/Calculator.java
@@ -1,90 +1,18 @@
-import java.util.Scanner;
-
-public class calculator {
-
-   private static Scanner scan = new Scanner(System.in);
+public class Calculator {
 
     public static void main (String[] args){
 
-        design();
-        System.out.println("Hello! My name is Pavel. This is my first program 'Calculator'.");
-        System.out.println("The program makes simple calculations with numbers.");
-        design();
+        CalcMethotd calcMethotd = new CalcMethotd();
 
-        System.out.println("Enter the number 1 ==> ");
-        double num1 = getNum();
+        System.out.print("Wprowadź pierwszą liczbę ==> ");
+        double num1 = calcMethotd.getNum();
 
-        System.out.println("Enter the number 2 ==> ");
-        double num2 = getNum();
+        System.out.print("Wprowadź drugą liczbę ==> ");
+        double num2 = calcMethotd.getNum();
 
-        System.out.println("Enter the operation (+, -, *, /) ==> ");
-        char operation = getOperation();
-        double result = calc(num1, num2, operation);
+        char operation = calcMethotd.getOperation();
+        double result = calcMethotd.calc(num1, num2, operation);
 
-        design();
-        System.out.println("Result: " + num1 + " " + operation + " " + num2 + " = " + result);
-        design();
+        System.out.println("Wynik: " + num1 + " " + operation + " " + num2 + " = " + result);
     }
-
-    public static void design(){
-        for (int i = 0; i <= 62; i++){
-            System.out.print("=");
-        }
-        System.out.println(" ");
-    }
-
-    public static double getNum(){
-
-        if (scan.hasNextDouble()) {
-            return scan.nextDouble();
-        } else
-        { System.out.println("Sorry, an error occurred while typing. Try again.");
-            return getNum();
-        }
-    }
-
-    public static char getOperation (){
-
-
-        char operation;
-
-        if (scan.hasNext()) {
-            operation = scan.next().charAt(0);
-        } else
-        { System.out.println("Sorry, an error occurred while typing. Try again.");
-            scan.next();
-            operation = getOperation();
-        }
-        return operation;
-    }
-
-    public static double calc(double num1, double num2, char operation){
-
-        double result;
-
-        switch (operation) {
-            case '+':
-                result = num1 + num2;
-                break;
-            case '-':
-                result = num1 - num2;
-                break;
-            case '*':
-                result = num1 * num2;
-                break;
-            case '/':
-                if (num2 != 0) {
-                    result = num1 / num2;
-                    break;
-                } else
-                    System.out.println("Sorry, you can not divide it by zero! Try again.");
-            default:
-                result = calc(getNum(), getNum(), getOperation());
-        }
-            return result;
-
-
-    }
-
-
 }
